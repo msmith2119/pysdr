50111aaf64beded59c91684c4e630588b0b8c425
morgan_elecraft <morgan@elecraft.com>
2025-11-12 15:16:21 -0800

    add driver script

--- run_iir.py
+++ run_iir.py
@@ -1,70 +1,61 @@
-from mydsp import Generators
-from mydsp.SigClasses import PlaySignals
-from scipy.fftpack import fft, ifft
-from mydsp.Filters import *
-
-import matplotlib.pyplot as plt
-from math import *
 import time
 
+from mydsp import Generators, Filters, SigClasses
+from mydsp.FFTLPFilter import FFTLPFilter
+from mydsp.FFTNotchFilter import FFTNotchFilter
+from mydsp.Filters import *
+from mydsp.LPFilter import LPFilter
+from mydsp.NotchFilter import NotchFilter
+from mydsp.SincLPFilter import SincLPFilter
+from mydsp.WavFileSink import WavFileSink
+from mydsp.WavFileSource import WavFileSource
+
 frame_size = 1000
-def hann(n):
-    return sin(pi*n/frame_size)**2
-w = np.zeros(frame_size)
-for i in range(0,frame_size):
-    w[i] = hann(i)
-
-#scq = Signal.from_file("count.wav")
-
 fs = 8000
 
-#s1 = scq[0].extract(0,8000)
-#s1 = Generators.sineWave("sine",0.01,200,fs,0,1.0)
-s2 = Generators.sineWave("sine2",0.5,800,fs,0,1.0)
-#s2 = scq[0]
-#s2 = Generators.unitWave("unit",8000,0,1.0)
-#print(s2.name)
-#sin = s1.add(s2)
-simpl = Generators.deltaWave("impulse",fs,0.01)
-#s2 = simpl
-#sin.plotFreq()
-#sin.name="Input"
-#sin.plotTime(tmin=0.12,tmax=0.13)
-#sin.plotFreq()
-f = SincLPF("sincf",8000,800.0,20)
-#f = AnalogFilter("avg",np.array([.33,.33,.33]),np.array([1]))
-#f = UnitFilter("unit",8000,800,1)
-#print(f.a)
-#print(sin.y[0:10])
-#print(f.size)
-#f = ButterworthFilter("b",800.0,8000.0,4)
+scq = Signal.from_file("audio/cqi.wav")
+#s2 = Generators.sineWave("sine2",0.5,1200,fs,0,1.0)
+s2 = scq[0]
 
-#imp.plotTime()
-#plt.figure()
+#f = SincLPFilter("sincf", 8000, 800.0, 20,1000)
+#f = LPFilter("fft",8000,800,frame_size)
+#f = FFTNotchFilter("notch",fs,1000,100,frame_size + 200)
+#f = AnalogFilter("simp",fs,np.array([1,1]),np.array([1]))
+f = NotchFilter("notch",fs,1000,100,frame_size)
+#f.impulse().plotTime()
 #f.plot(1)
-#s3.plotFreq(logscale=True)
-#s3.plotTime()
-#scq[0].plotTime()
-#s5 = f.processfft(sin,frame_size)
-s2.plotTime()
-f.setWindowType(WindowType.HANN)
-#s5 = f.processfft(s2,frame_size)
-start_time = time.time()
-s5 = f.processConv(s2,frame_size)
-#s5 = f.process(s2)
-end_time = time.time()
-elapsed_time = end_time - start_time
+f.plotFFT()
+#start_time = time.time()
+#s5 = f.processConv(s2,frame_size)
+s2.frame_size = frame_size
+
+
+source = WavFileSource("audio/cqi.wav",frame_size)
+sink = WavFileSink("iir_out.wav", frame_size, sample_rate=fs)
+while True:
+    frame = source.getFrame()
+    if frame is None:
+          break
+    out_frame = f.doFrame(frame)
+    if out_frame is not None:
+         sink.writeFrame(out_frame)
+sink.writeFrame(f.prevResult)
+sink.close()
+source.close()
 
 #s5 = f.process(s2)
-#s5 = f.process(s2)
-s5.name="Output"
-s5.plotFreq()
-s5.plotTime(stick=False,tmin=0,tmax=0.14)
-s5.plotTime()
-#sin.plotTime(stick=False,tmax=0.2)
-sout=[s5,s5]
+#s5 = f.processConv(s2)
+#end_time = time.time()
+#elapsed_time = end_time - start_time
+
+#s5.name="Output"
+#s5.plotFreq()
+
+#s5.plotTime(stick=False,tmin=0,tmax=0.14)
+#s5.plotTime()
+#sout=[s5,s5]
 #Signal.to_file(sout,"iir_out.wav")
-print(f"Filter time: {elapsed_time:.6f} seconds")
-f.plotWindow(1000)
+#print(f"Filter time: {elapsed_time:.6f} seconds")
+#f.plotEnvelope(1000)
 plt.show()
 
